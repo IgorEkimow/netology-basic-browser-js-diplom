e3c8e910fe340674866212a78238cb1771f30076
Igor <igorekimow@gmail.com>
2020-11-26 00:20:22 +0300

    Modal/Sidebar Done

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,7 +12,11 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    if (!element) {
+      throw new Error("Ошибка! Элемент не существует!");
+    }
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -21,7 +25,12 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+    const modalDissmiss = this.element.querySelectorAll(`button[data-dismiss="modal"]`);
+    for (let modal of modalDissmiss) {
+      modal.addEventListener("click", event => {
+        this.onClose(event);
+      });
+    }
   }
 
   /**
@@ -29,25 +38,26 @@ class Modal {
    * Закрывает текущее окно (Modal.close())
    * */
   onClose( e ) {
-
+    e.preventDefault();
+    this.close();
   }
   /**
    * Удаляет обработчики событий
    * */
   unregisterEvents() {
-
+    this.element.removeEventListener("click", this.onClose);
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = "block";
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.display = "";
   }
 }
--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,13 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
-
+    const button = document.querySelector(".sidebar-mini");
+    const toggle = document.querySelector(".sidebar-toggle");
+    toggle.addEventListener("click", event => {
+      event.preventDefault();
+      button.classList.toggle("sidebar-open");
+      button.classList.toggle("sidebar-collapse");
+    });
   }
 
   /**
@@ -29,7 +35,26 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
+    const register = document.querySelector(".menu-item_register");
+    register.addEventListener("click", event => {
+      event.preventDefault();
+      App.getModal("register").open();
+    });
 
+    const login = document.querySelector(".menu-item_login");
+    login.addEventListener("click", event => {
+      event.preventDefault();
+      App.getModal("login").open();
+    });
+
+    const logout = document.querySelector(".menu-item_logout");
+    logout.addEventListener("click", event => {
+      event.preventDefault();
+      User.logout({}, (err, response) => {
+        if (response.success) {
+          App.setState("init");
+        }
+      });
+    });
   }
-
 }
